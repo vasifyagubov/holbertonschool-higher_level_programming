0075526ce835e476c548a7e4b7ca2fddca82c8fe
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-09-10 13:54:20 +0400

    no c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@
 def no_c(my_string):
     for char in my_string:
         if char != 'c' and char != 'C':
-            result += char
-    return result
+            result.append(char)
+    return ''.join(result)
