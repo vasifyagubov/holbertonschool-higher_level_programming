24092f5382788b1b88ef7da663e390ff63eecf58
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-09-10 21:54:34 +0400

    matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,4 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(' '.join('{:d'}.format(num) for num in row)
-        
+        print(' '.join('{:d}'.format(num) for num in row)
