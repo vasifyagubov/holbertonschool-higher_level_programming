474b9ff40b0d1e8eaa9e0639636d50d8de491748
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-09-11 19:46:08 +0400

    best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(a_dictionary, key = a_dictionary.get)
+        return max(a_dictionary, key=a_dictionary.get)
     else:
         return None
     
