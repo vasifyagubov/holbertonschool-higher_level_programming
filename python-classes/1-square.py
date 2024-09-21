ec4604b1c1df94ded0aaf66aba5b27a6e426aa0a
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-09-22 01:43:37 +0400

    self

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Define Square Class For Equaling Size"""
+"""
+A Square class that defines a square
+"""
 
 
 class Square:
-    """Class of square"""
-
-    def __init__(self, __size):
-        """__init__ method that equals size
-
-        Args:
-            size (int): size of square
-        """
-        self.__size = __size        
+    """Square class that define a square with is self size"""
+    def __init__(self, size):
+        """instance attribute"""
+        self.__size = size
