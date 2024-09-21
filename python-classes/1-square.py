1312a65e05e0fa58bc26e02f947a520db4b737b9
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-09-22 01:27:19 +0400

    classss

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+A Square class that defines a square
+"""
+
 
 class Square:
-   def __init__(self, __size):
-      self.__size = __size
-       
+    """Square class that define a square with is self size"""
+    def __init__(self, size):
+        """instance attribute"""
+        self.__size = size
+        
