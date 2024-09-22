49f339cbd2415ced817a99fc41cb7bfacfb4faab
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-09-22 12:14:09 +0000

    Class

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -67,6 +67,7 @@ class Rectangle:
     def __repr__(self):
         """Method to return the string representation of the rectangle"""
         return "Rectangle({}, {})".format(self.width, self.height)
+
     def __del__(self):
         """deleting rectangle"""
         print("Bye rectangle...")
