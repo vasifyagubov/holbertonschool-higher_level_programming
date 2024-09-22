50c58f13ba0a70899f4fc5b91c1c448588d457bf
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-09-22 07:36:24 +0000

    Class

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a class Square with private instance attribute size."""
+
+
+class Square:
+    """Square class with private attribute size"""
+    def __init__(self, size=0):
+        """Initialize Square with size attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """"Area definer"""
+        return self.__size ** 2
