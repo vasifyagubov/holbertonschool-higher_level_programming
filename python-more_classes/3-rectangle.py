2a878d2f1acebe5510ce052e67d77b63aa7feb7c
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-09-22 11:46:19 +0000

    Class

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -59,4 +59,4 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(["#" * self.__width for in range (self.__height)])
+        return "\n".join(["#" * self.__width for i in range (self.__height)])
