fe71c20fa69a75877027c7b82770ded5e481e418
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-09-29 10:51:31 +0000

    Hbtn

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,6 +2,10 @@
 """Module containing print_sorted func"""
 
 class Mylist(list):
+
+    """Class inherited from list class"""
+
     def print_sorted(self):
+        """Method to print sorted list"""
         sorted_list = sorted(self)
         print(sorted_list)
