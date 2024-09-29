1b23dff3020d9680f16a0e54a6a975314eb8d9e4
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-09-29 11:08:03 +0000

    Hbtn

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write and empty class"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def area(self):
+        """Functionnn"""
+        raise Exception("area() is not implemented")
