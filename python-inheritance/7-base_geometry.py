b4386d2b15668654853d79a4fae2cdcadee1fd62
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-09-29 11:15:59 +0000

    Hbtn

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,6 +7,7 @@ class BaseGeometry:
     def area(self):
         """Functionnn"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """Integer"""
         if type(value) is not int:
