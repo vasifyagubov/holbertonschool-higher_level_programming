914e9b0df87e59895823f3721c850e98ede61f04
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-10-06 07:36:37 +0000

    Git

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Function of file"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        a = json.dumps(my_obj)
+        return f.read(a)
