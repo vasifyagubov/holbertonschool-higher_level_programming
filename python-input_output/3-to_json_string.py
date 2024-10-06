7f71827874bb88921c18f10aeddcf588d2341b4e
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-10-06 07:13:00 +0000

    Git

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""To json string"""
+import json
+
+
+def to_json_string(my_obj):
+    """Function"""
+    return json.dumps(my_obj)
---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""from json string to object"""
+import json
+
+
+def from_json_string(my_str):
+    """Function"""
+    return json.loads(my_str)
