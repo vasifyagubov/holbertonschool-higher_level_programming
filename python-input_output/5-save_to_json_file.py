3f9ca9d47ac0229c75e11ccc7abe1afe1b76229f
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-10-06 07:31:26 +0000

    Git

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Function"""
-    with open(filename, encoding='utf8') as file:
+    with open(filename, 'w', encoding='utf8') as file:
         a=json.dumps(my_obj)
         return (file.write(a))
