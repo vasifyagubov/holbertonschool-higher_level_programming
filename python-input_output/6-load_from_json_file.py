058eebdf583fa847b42271259dda8b2c06b4f91a
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-10-06 07:37:30 +0000

    Git

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """Function of file"""
     with open(filename, 'r', encoding='utf-8') as f:
-        a = json.dumps(my_obj)
+        a = json.loads(my_obj)
         return f.read(a)
