d728ff691472f3d0f527c8bc5e5dd7343225b750
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-10-06 07:41:30 +0000

    Git

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,5 +6,4 @@ import json
 def load_from_json_file(filename):
     """function of getting json file string to object"""
     with open(filename, 'r', encoding='utf-8') as f:
-        a = json.loads(my_obj)
-        return f.read(a)
+        return json.loads(f.read())
