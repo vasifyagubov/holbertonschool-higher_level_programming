30b21df82f66001f0a53ee8905582cb31a3eda38
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-10-20 09:55:52 +0000

    Request

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import json
+import http.server
+
+class Basicserver(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+        elif self.path == '\data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content_type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Endpoint not found')
+
+server = ('', 8000)
+httpd = http.server.HTTPServer(server, BasicServer)
+print('Server is running on port 8000...')
+httpd.serve_forever()
