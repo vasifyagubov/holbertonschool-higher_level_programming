46d0eddd6eefca0326df25fae170a2cb81a09bee
vasifyagubov <vasif.yaqubov85@gmail.com>
2024-11-02 13:27:00 +0000

    SQL learn

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,10 @@
--- Full creatioN
-CREATE TABLE IF NOT EXISTS second_table(id, name, score) VALUES (INT, VARCHAR(256), INT);
+-- Full creation
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT);
 
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
 INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
-INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES (4, "George",i 8);
